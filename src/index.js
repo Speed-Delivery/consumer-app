1e064fdf5033876bdcebb730c3390cd0d3f0770d
Muna Mussa <munammahdi@gmail.com>
2023-11-18 20:18:27 +0200

    fixed to react 18

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 // src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css'; // Import your Tailwind CSS file
 import App from './App';
 import './App.css'; // Import your App.css containing Tailwind CSS
 
-
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
