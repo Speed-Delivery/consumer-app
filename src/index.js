8f355de14495db0ad29a8e8d19916666ddcbee3e
Muna Mussa <munammahdi@gmail.com>
2023-11-19 21:26:41 +0200

    using context API

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css'; // Import your Tailwind CSS file
 import App from './App';
 import './App.css'; // Import your App.css containing Tailwind CSS
-
+import { UserProvider } from './components/context/UserContext';
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </React.StrictMode>
 );
