780f8995c17d151ffd162f1268d131c0efe5dab8
Muna Mussa <munammahdi@gmail.com>
2023-11-19 21:26:00 +0200

    using context API

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,59 +1,17 @@
-import React, { useEffect, useState } from "react";
-
-import Login from "./user/Login";
-import Signup from "./user/Signup";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "./context/UserContext";
 
-const UserProfile = ({ setIsAuthenticated }) => {
-  const [showLogin, setShowLogin] = useState(true);
+const UserProfile = () => {
+  const { user, isAuthenticated } = useContext(UserContext);
 
-  const [user, setUser] = useState({
-    userId: "",
-    username: "",
-    fullName: "",
-    email: "",
-    phone: "",
-    role: "",
-    address: "",
-  });
-
-  useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem("user"));
-    const storedAuth = JSON.parse(localStorage.getItem("isAuthenticated"));
-
-    if (storedUser && storedAuth) {
-      setUser(storedUser);
-      setIsAuthenticated(storedAuth);
-    } else {
-      //if the data is not available in local storage, fetch it from the server
-      fetch("http://localhost:5005/api/user/profile", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(storedUser._id),
-      })
-        .then((res) => {
-          if (res.ok) {
-            const data = res.json();
-            console.log(data);
-            setUser(data);
-          }
-        })
-        .catch((err) => {
-          console.error("There was an error.", err);
-        });
-    }
-  }, []);
+ 
+  if (!isAuthenticated) {
+    // Redirect to login page or show a message if the user is not authenticated
+    return <div>You need to login to view this page.</div>;
+  }
 
   return (
-    // <div>
-    //   {showLogin ? (
-    //     <Login onSwitch={() => setShowLogin(false)} />
-    //   ) : (
-    //     <Signup onSwitch={() => setShowLogin(true)} />
-    //   )}
-    // </div>
     <div className="bg-gray-100 flex justify-center items-center min-h-screen">
       <div className="bg-white shadow-2xl rounded-2xl p-4 w-8/12 max-w-md">
         <h1 className="text-center font-semibold text-2xl py-2">
@@ -66,12 +24,8 @@ const UserProfile = ({ setIsAuthenticated }) => {
               <td className="border px-4 py-2">{user.fullName}</td>
             </tr>
             <tr>
-              <td className="border px-4 py-2">User Name</td>
+              <td className="border px-4 py-2">Username</td>
               <td className="border px-4 py-2">{user.username}</td>
-            </tr>
-            <tr>
-              <td className="border px-4 py-2">User ID</td>
-              <td className="border px-4 py-2">{user._id}</td>
             </tr>
             <tr>
               <td className="border px-4 py-2">Email</td>
@@ -91,7 +45,7 @@ const UserProfile = ({ setIsAuthenticated }) => {
             </tr>
           </tbody>
         </table>
-        <Link to={"/edit-profile"}>
+        <Link to="/edit-profile">
           <button className="btn py-2 my-2 bg-black text-white w-full">
             Update Information
           </button>
