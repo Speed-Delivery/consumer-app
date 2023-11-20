5c3437055de6fea4aabaaab10a1d04b742b24c1e
Muna Mussa <munammahdi@gmail.com>
2023-11-20 20:08:43 +0200

    fix context api integration

--- src/App.js
+++ src/App.js
@@ -14,62 +14,12 @@ import EditUserProfile from "./components/EditUserProfile";
 import {  UserContext } from "./components/context/UserContext";
 
 const App = () => {
-  const { user, updateUser, isAuthenticated, setIsAuthenticated } = useContext(UserContext);
+  const { user, isAuthenticated, setIsAuthenticated } = useContext(UserContext);
 
   console.log("Current User: ", user);
   console.log("Is Authenticated: ", isAuthenticated);
-  const handleLogin = async (credentials) => {
-    try {
-      const response = await fetch("http://localhost:5005/api/users/signin", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(credentials),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        updateUser(data);
-        setIsAuthenticated(true);
-        localStorage.setItem("user", JSON.stringify(data));
-        localStorage.setItem("isAuthenticated", JSON.stringify(true));
-      } else {
-        console.error("Login failed.");
-      }
-    } catch (err) {
-      console.error("There was an error.", err);
-    }
-  };
-
-  const onSignup = async (formData) => {
-    try {
-      const response = await fetch('http://localhost:5005/api/users', {
-        method: 'POST',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        setIsAuthenticated(true);
-
-        // Update user state using updateUser from Context
-        updateUser(data);  // Assuming the response returns the user object
-
-        // Update local storage
-        localStorage.setItem("user", JSON.stringify(data));
-        localStorage.setItem("isAuthenticated", JSON.stringify(true));
-      } else {
-        console.error("Signup failed");
-      }
-    } catch (error) {
-      console.error("There was an error signing up", error);
-    }
-  };
-
+  
+  
   const handleSignOut = () => {
     setIsAuthenticated(false);
     localStorage.removeItem("user");
@@ -78,25 +28,25 @@ const App = () => {
 
 
   return (
-      <Router>
-        <Navbar
-          user={user}
-          isAuthenticated={isAuthenticated}
-          onSignOut={handleSignOut}
-        />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={!isAuthenticated ? <Login onAuthenticated={handleLogin} /> : <Navigate to="/" />} />
-          <Route path="/signup" element={!isAuthenticated ? <Signup onSignup={onSignup} /> : <Navigate to="/" />} />
-          {isAuthenticated && <Route path="/user-profile" element={<UserProfile />} />}
-          {isAuthenticated && <Route path="/edit-profile" element={<EditUserProfile />} />}
-          {isAuthenticated && <Route path="/parcel-history" element={<ParcelHistory />} />}
-          {isAuthenticated && <Route path="/send-parcel" element={<SendParcel />} />}
-          {isAuthenticated && <Route path="/notifications" element={<Notifications />} />}
-          {user?.role === "admin" && <Route path="/admin-panel" element={<AdminPanel />} />}
-        </Routes>
-        <Footer />
-      </Router>
+    <Router>
+      <Navbar
+        user={user}
+        isAuthenticated={isAuthenticated}
+        onSignOut={handleSignOut}
+      />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={!isAuthenticated ? <Login /> : <Navigate to="/" />} />
+        <Route path="/signup" element={!isAuthenticated ? <Signup /> : <Navigate to="/" />} />
+        {isAuthenticated && <Route path="/user-profile" element={<UserProfile />} />}
+        {isAuthenticated && <Route path="/edit-profile" element={<EditUserProfile />} />}
+        {isAuthenticated && <Route path="/parcel-history" element={<ParcelHistory />} />}
+        {isAuthenticated && <Route path="/send-parcel" element={<SendParcel />} />}
+        {isAuthenticated && <Route path="/notifications" element={<Notifications />} />}
+        {user?.role === "admin" && <Route path="/admin-panel" element={<AdminPanel />} />}
+      </Routes>
+      <Footer />
+    </Router>
   );
 };
 
