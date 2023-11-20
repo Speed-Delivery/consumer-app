dde3d2e9ced3f084b276114ae443f99b9b5534a5
matusala3 <matusalakiflom@gmail.com>
2023-11-20 23:38:36 +0200

    added parcel and modified locker system

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,12 @@ import React from "react";
 import Locker from "../components/lockers/Locker";
 
 const Home = () => {
-  const lockerCount = 15;
-  const lockers = Array.from({ length: lockerCount }, (_, index) => index + 1); // create an array of 15 lockers with unique IDs
-
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold text-center mb-8">Parcel Lockers</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {lockers.map((id) => (
-          <Locker key={id} id={id} />
-        ))}
-      </div>
+    <div className="container mx-auto p-6 md:p-8">
+      <h1 className="text-3xl md:text-4xl font-bold text-center text-gray-800 mb-10">
+        Parcel Lockers
+      </h1>
+      <Locker />
     </div>
   );
 };
