37a46b7152f43fe65fb74c7a063c71dfb434c996
matusala3 <matusalakiflom@gmail.com>
2023-12-08 01:05:45 +0200

    v2-01

--- src/components/history/ParcelHistory.js
+++ src/components/history/ParcelHistory.js
@@ -4,89 +4,104 @@ import { UserContext } from "../context/UserContext";
 
 const ParcelHistory = () => {
   const { user } = useContext(UserContext);
-  console.log(user, "from parcel history");
 
   const [data, setData] = useState([]);
+
   const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
-    // Make a GET request to fetch the array of objects
-    fetch("http://localhost:5005/api/parcels") // Replace with your API endpoint
+    // Retrieve the user ID from local storage
+    const userId = JSON.parse(localStorage.getItem("user"))._id;
+
+    fetch("http://localhost:5005/api/parcels")
       .then((response) => response.json())
       .then((result) => {
         console.log(result, "result after response");
 
-        // Check if the result has a 'parcels' property
         if (result.parcels && Array.isArray(result.parcels)) {
-          const parcelsArray = result.parcels;
-
-          // Filter the data based on the specified conditions
-          const filteredResult = parcelsArray.filter((item) => {
-            // Example: Filter based on sender's name and email
-            return (
-              (item.sender &&
-                item.sender.name === user.username &&
-                item.sender.email === user.email) ||
-              (item.recipient &&
-                item.recipient.name === user.username &&
-                item.recipient.email === user.email)
-            );
+          // Filter parcels where the current user is the sender
+          const filteredResult = result.parcels.filter((parcel) => {
+            return parcel.sender && parcel.sender.user === userId;
           });
 
-          setData(parcelsArray);
-          setFilteredData(filteredResult);
+          setData(result.parcels); // Set all parcel data
+          setFilteredData(filteredResult); // Set filtered data for parcels sent by the current user
           console.log(filteredResult, "filtered result");
         } else {
           console.error("Unexpected data structure:", result);
         }
       })
       .catch((error) => console.error("Error fetching data:", error));
-  }, [user]);
-  // Function to format the date
-  // Function to format the date
-  const formatCreatedAt = (createdAt) => {
-    const createdAtDate = new Date(createdAt);
+  }, []); // Empty dependency array as this effect doesn't depend on any changing values
 
-    // Format the date components manually
-    const year = createdAtDate.getFullYear();
-    const month = String(createdAtDate.getMonth() + 1).padStart(2, "0");
-    const day = String(createdAtDate.getDate()).padStart(2, "0");
-    const hours = String(createdAtDate.getHours()).padStart(2, "0");
-    const minutes = String(createdAtDate.getMinutes()).padStart(2, "0");
-    const seconds = String(createdAtDate.getSeconds()).padStart(2, "0");
-
-    // Create the formatted date string
-    const formattedDateString = `${day}-${month}-${year} `;
-
-    return formattedDateString;
-  };
+  // Dependency array includes user to re-run effect when user changes
 
   return (
     <div className="container mx-auto p-4">
-      <h2 className=" text-4xl font-bold text-center my-8">
+      <h2 className="text-4xl font-bold text-center my-8 text-blue-600">
         Parcel Information and History
       </h2>
-      <div className="mt-4">
-        {filteredData.map((parcel, index) => (
-          <div key={index} className="border rounded p-4 mb-4">
-            <p>
-              <strong>Sender:</strong> {parcel.sender.name}
-            </p>
-            <p>
-              <strong>Recipient:</strong> {parcel.recipient.name}
-            </p>
-            <p>
-              <strong>Weight:</strong> {parcel.parcelWeight}
-            </p>
-            <p>
-              <strong>Sent at:</strong>
-              {formatCreatedAt(parcel.createdAt)}
-            </p>
-            <p>
-              <strong>Description:</strong> {parcel.parcelDescription}
-            </p>
-          </div>
-        ))}
+      <div>
+        {filteredData.length > 0 ? (
+          filteredData.map((parcel, index) => (
+            <div
+              key={index}
+              className="mb-6 p-5 border rounded-lg shadow-lg bg-white hover:shadow-xl transition-shadow"
+            >
+              <h3 className="text-2xl font-semibold text-purple-700 mb-2">
+                Parcel {index + 1}
+              </h3>
+              <p className="text-md mb-1">
+                <strong>Sender:</strong>{" "}
+                <span className="text-gray-600">{parcel.sender.name}</span>
+              </p>
+              <p className="text-md mb-1">
+                <strong>Recipient:</strong>{" "}
+                <span className="text-gray-600">{parcel.recipient.name}</span>
+              </p>
+              <p className="text-md mb-1">
+                <strong>Ready for Pickup:</strong>{" "}
+                <span className="text-green-500">
+                  {parcel.readyForPickupDate}
+                </span>
+              </p>
+              <p className="text-md mb-1">
+                <strong>Status:</strong>{" "}
+                <span className="text-blue-500">{}</span>
+              </p>
+              <p className="text-md mb-3">
+                <strong>Status:</strong>{" "}
+                <span
+                  className={`font-semibold ${
+                    parcel.status === "Ready for Pickup"
+                      ? "text-red-500"
+                      : "text-indigo-500"
+                  }`}
+                >
+                  {parcel.status}
+                </span>
+              </p>
+              {parcel.status === "Ready for Pickup" && (
+                <div className="bg-yellow-100 p-3 rounded border border-yellow-200">
+                  <p>
+                    <strong>Location:</strong>{" "}
+                    <span className="text-gray-700">
+                      {parcel.pickupLocation}
+                    </span>
+                  </p>
+                  <p>
+                    <strong>Pickup Code:</strong>{" "}
+                    <span className="text-gray-700">{parcel.pickupCode}</span>
+                  </p>
+                </div>
+              )}
+            </div>
+          ))
+        ) : (
+          <p className="text-center text-gray-500">
+            No parcel history available.
+          </p>
+        )}
       </div>
     </div>
   );
