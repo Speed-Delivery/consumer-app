eb22e410854706b7e82a719e275c4be1461aac21
Muna Mahadi <munammahadi@gmail.com>
2023-12-10 14:11:00 +0200

    notiofication fixed

--- src/components/notifications/Notification.js
+++ src/components/notifications/Notification.js
@@ -1,62 +1,130 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState, useEffect } from 'react';
+import NotificationCard from './NotificationCard'; // Import the component
+ 
 const Notification = () => {
+  const [parcels, setParcels] = useState([]);
   const [transactions, setTransactions] = useState([]);
-
-  // Fetch transactions data from the server and filter by user ID
-  const fetchTransactions = async () => {
-    // Get the user ID from local storage
+  const [lockers, setLockers] = useState([]);
+  const [matchingData, setMatchingData] = useState({
+    matchingParcels: [],
+    matchingTransactions: [],
+    matchingLockers: [],
+    matchingCabinets: [],
+  });
+ 
+  useEffect(() => {
+    // Step 1: Retrieve the user ID from local storage
     const userId = JSON.parse(localStorage.getItem("user"))._id;
-    if (!userId) {
-      console.error("User ID not found in local storage");
-      return;
-    }
-
-    try {
-      const response = await fetch("http://localhost:5005/api/transactions/");
-      const data = await response.json();
-
-      // Filter transactions for the current user
-      const userTransactions = data.transactions.filter(
-        (transaction) => transaction.userId === userId
-      );
-      setTransactions(userTransactions);
-    } catch (error) {
-      console.error("Error fetching transactions:", error);
+ 
+    // Step 2: Fetch parcels where the current user is the sender
+    fetch("http://localhost:5005/api/parcels")
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.parcels && Array.isArray(result.parcels)) {
+          // Filter parcels where the current user is the sender
+          const filteredResult = result.parcels.filter((parcel) => {
+            return parcel.sender && parcel.sender.user === userId;
+          });
+ 
+          setParcels(filteredResult); // Set filtered parcel data
+        } else {
+          console.error("Unexpected data structure for parcels:", result);
+        }
+      })
+      .catch((error) => console.error("Error fetching parcels data:", error));
+  }, []); // Empty dependency array means this effect runs once after initial render
+ 
+  useEffect(() => {
+    // Step 3: Fetch all transactions
+    fetch("http://localhost:5005/api/transactions/")
+    .then((response) => response.json())
+    .then((transactionData) => {
+      console.log("Fetched transactions:", transactionData.transactions); // Add this line
+      if (Array.isArray(transactionData.transactions)) {
+        setTransactions(transactionData.transactions);
+      } else {
+        console.error("Transaction data is not an array:", transactionData);
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching transaction data:", error);
+    });
+  
+  }, []);
+ 
+  useEffect(() => {
+    // Step 4: Fetch lockers data
+    fetchLockers();
+  }, [parcels]);
+ 
+  const fetchLockers = async () => {
+    const response = await fetch("http://localhost:5005/api/lockers");
+    const data = await response.json();
+    if (data && Array.isArray(data.lockers)) {
+      setLockers(data.lockers);
+    } else {
+      console.error("Data is not in the expected format:", data);
     }
   };
-
+ 
   useEffect(() => {
-    fetchTransactions();
-  }, []);
-
+    // Step 5: Match cabinets to lockers
+    if (parcels.length > 0 && lockers.length > 0) {
+      const parcelCabinetMapping = {
+        matchingParcels: [],
+        matchingTransactions: [],
+        matchingLockers: [],
+        matchingCabinets: [],
+      };
+ 
+      transactions.forEach((transaction) => {
+        // Check if the transaction status is 'awaiting pickup'
+        if (transaction.parcelStatus === "awaiting pickup") {
+          const parcelId = transaction.parcelId;
+          const cabinetId = transaction.CabinetId;
+      
+          const matchingParcel = parcels.find((parcel) => parcel._id === parcelId);
+      
+          if (matchingParcel) {
+            const lockerId = lockers.find((locker) =>
+              locker.cabinets.some((cabinet) => cabinet.id === cabinetId)
+            )?.id;
+      
+            if (lockerId) {
+              const matchingCabinet = lockers
+                .find((locker) => locker.id === lockerId)
+                .cabinets.find((cabinet) => cabinet.id === cabinetId);
+      
+              if (matchingCabinet) {
+                parcelCabinetMapping.matchingCabinets.push({
+                  ...matchingCabinet,
+                  transactionStatus: transaction.parcelStatus
+                });
+              }
+            }
+          }
+        }
+      });
+ 
+      setMatchingData(parcelCabinetMapping);
+    }
+  }, [parcels, lockers, transactions]);
+ 
   return (
-    <>
-      {transactions.length > 0 ? (
-        <div className="bg-gray-100 flex justify-center items-center min-h-screen">
-          <div className="bg-white shadow-2xl rounded-2xl p-4 w-8/12 max-w-md">
-            <h1 className="text-center font-semibold text-2xl py-2">
-              Your Transactions
-            </h1>
-            {transactions.map((transaction, index) => (
-              <div
-                className="bg-white shadow-md rounded-xl p-4 my-2"
-                key={index}
-              >
-                <p>Transaction ID: {transaction._id}</p>
-                <p>Amount: {transaction.amount}</p>
-                <p>Date: {new Date(transaction.date).toLocaleDateString()}</p>
-                <p>Status: {transaction.status}</p>
-                {/* Add any other transaction details you need to display */}
-              </div>
-            ))}
-          </div>
-        </div>
-      ) : (
-        <p>No transactions found for this user.</p>
-      )}
-    </>
+    <div>
+      <NotificationCard
+        title="Matching Cabinets"
+        items={matchingData.matchingCabinets.map((cabinet) => ({
+          cabinetNumber: cabinet.cabinetNumber,
+          status: cabinet.transactionStatus,
+          code: cabinet.code,
+          cabinetStatusLastUpdated: cabinet.cabinetStatusLastUpdated,
+        }))}
+      />
+    </div>
   );
 };
-
+ 
 export default Notification;
+ 
+ 
