2925099af39ff2a70bec5321898ce3db30c0ac47
Muna Mussa <munammahdi@gmail.com>
2023-12-13 08:04:58 +0200

    env variables

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,3 +1,3 @@
 // src/apiConfig.js
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5005';
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://172.104.151.10:5005";
 export default API_BASE_URL;
